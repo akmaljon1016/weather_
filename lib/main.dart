4b0873a1ded053613e62eb6d5a01b18aebf6e37e
akmal1016 <akmaljonsotvoldiev1998@gmail.com>
2023-09-11 15:56:35 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:weather/user.dart';
+import 'package:weather/weather.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -18,10 +19,11 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   Dio dio = Dio();
 
-  Future<List<User>> getUser() async {
-    var response = await dio.get("https://jsonplaceholder.typicode.com/users");
+  Future<Weather> getWeather() async {
+    var response = await dio.get(
+        "https://api.weatherapi.com/v1/current.json?key=acb4a4de25aa41b784651422200510&q=Margilan");
     if (response.statusCode == 200) {
-      return listFromJson(response.data);
+      return Weather.fromJson(response.data);
     } else {
       throw Exception;
     }
@@ -34,26 +36,27 @@ class _MyAppState extends State<MyApp> {
           title: Text("User download"),
         ),
         body: FutureBuilder(
-            future: getUser(),
-            builder:
-                (BuildContext context, AsyncSnapshot<List<User>> snapshot) {
+            future: getWeather(),
+            builder: (BuildContext context, AsyncSnapshot<Weather> snapshot) {
               if (snapshot.hasData) {
-                return ListView.builder(
-                  itemCount: snapshot.data?.length,
-                    itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    margin: EdgeInsets.all(10),
-                    color: Colors.green[100],
+                return Center(
+                  child: Container(
+                    width: 200,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.green[200],
+                    ),
                     child: Column(
                       children: [
-                        Text(snapshot.data?[index].company?.name ??
-                            "this is empty"),
-                        Text(snapshot.data?[index].name ??
-                            "this is empty"),
+                        Image.network("https:" +
+                            (snapshot.data?.current?.condition?.icon ?? ""),width: 100,height: 100,),
+                        Text(
+                            snapshot.data?.current?.tempC.toString() ?? "empty",style: TextStyle(fontSize: 30),)
                       ],
                     ),
-                  );
-                });
+                  ),
+                );
               } else {
                 return Center(
                   child: CircularProgressIndicator(),
