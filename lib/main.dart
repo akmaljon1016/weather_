adb138d2df84449870510a4cdc48e2c91ec08a6f
akmal1016 <akmaljonsotvoldiev1998@gmail.com>
2023-09-13 16:33:38 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -34,90 +34,105 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text("User download"),
         ),
-        body: FutureBuilder(
-            future: getWeather(),
-            builder: (BuildContext context, AsyncSnapshot<Weather> snapshot) {
-              if (snapshot.hasData) {
-                return Container(
-                  width: MediaQuery.of(context).size.width,
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 200,
-                      ),
-                      Container(
-                        width: 200,
-                        height: 200,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.green[200],
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+            image: DecorationImage(image: AssetImage("assets/nature.jpeg"),fit: BoxFit.fill)
+          ),
+          child: FutureBuilder(
+              future: getWeather(),
+              builder: (BuildContext context, AsyncSnapshot<Weather> snapshot) {
+                if (snapshot.hasData) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 200,
                         ),
-                        child: Column(
-                          children: [
-                            Image.network(
-                              "https:" +
-                                  (snapshot.data?.current?.condition?.icon ??
-                                      ""),
-                              width: 100,
-                              height: 100,
-                            ),
-                            Text(
-                              snapshot.data?.current?.tempC.toString() ??
-                                  "empty",
-                              style: TextStyle(fontSize: 30),
-                            )
-                          ],
+                        Container(
+                          width: 200,
+                          height: 200,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.green[200],
+                          ),
+                          child: Column(
+                            children: [
+                              Image.network(
+                                "https:" +
+                                    (snapshot.data?.current?.condition?.icon ??
+                                        ""),
+                                width: 100,
+                                height: 100,
+                              ),
+                              Text(
+                                snapshot.data?.current?.tempC.toString() ??
+                                    "empty",
+                                style: TextStyle(fontSize: 30),
+                              )
+                            ],
+                          ),
                         ),
-                      ),
-                      Container(
-                        height: 200,
-                        child: ListView.builder(
-                            itemCount:
-                                snapshot.data?.forecast?.forecastday?.length,
-                            scrollDirection: Axis.horizontal,
-                            itemBuilder: (BuildContext context, int index) {
-                              return Container(
-                                margin: EdgeInsets.all(10),
-                                width: 200,
-                                height: 200,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(20),
-                                  color: Colors.green[200],
-                                ),
-                                child: Column(
-                                  children: [
-                                    Image.network(
-                                      "https:" +
-                                          (snapshot
-                                                  .data
-                                                  ?.forecast
-                                                  ?.forecastday?[index]
-                                                  .day
-                                                  ?.condition
-                                                  ?.icon ??
-                                              ""),
-                                      width: 100,
-                                      height: 100,
-                                    ),
-                                    Text(
-                                      snapshot.data?.forecast?.forecastday?[index].day?.avgtempC.toString() ??
-                                          "empty",
-                                      style: TextStyle(fontSize: 30),
-                                    ),
-                                    Text(snapshot.data?.forecast?.forecastday?[index].date??"empty")
-                                  ],
-                                ),
-                              );
-                            }),
-                      )
-                    ],
-                  ),
-                );
-              } else {
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-            }));
+                        Container(
+                          height: 200,
+                          child: ListView.builder(
+                              itemCount:
+                                  snapshot.data?.forecast?.forecastday?.length,
+                              scrollDirection: Axis.horizontal,
+                              itemBuilder: (BuildContext context, int index) {
+                                return Container(
+                                  margin: EdgeInsets.all(10),
+                                  width: 200,
+                                  height: 200,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20),
+                                    color: Colors.green[200],
+                                  ),
+                                  child: Column(
+                                    children: [
+                                      Image.network(
+                                        "https:" +
+                                            (snapshot
+                                                    .data
+                                                    ?.forecast
+                                                    ?.forecastday?[index]
+                                                    .day
+                                                    ?.condition
+                                                    ?.icon ??
+                                                ""),
+                                        width: 100,
+                                        height: 100,
+                                      ),
+                                      Text(
+                                        snapshot
+                                                .data
+                                                ?.forecast
+                                                ?.forecastday?[index]
+                                                .day
+                                                ?.avgtempC
+                                                .toString() ??
+                                            "empty",
+                                        style: TextStyle(fontSize: 30),
+                                      ),
+                                      Text(snapshot.data?.forecast
+                                              ?.forecastday?[index].date ??
+                                          "empty")
+                                    ],
+                                  ),
+                                );
+                              }),
+                        )
+                      ],
+                    ),
+                  );
+                } else {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              }),
+        ));
   }
 }
